30b9cb8c329c5aa45bb62dcccd5a7a56c8371a49
crahal <charlierahal@gmail.com>
2023-11-13 23:22:13 +0000

    Replicate Hofert (2020), expand for 64bit MT.

--- src/build_collisions.R
+++ src/build_collisions.R
@@ -1,11 +1,14 @@
+library(dqrng)
+
 seeds <- as.integer(readLines(file.path("..", "assets", "seed_list.txt")))
 
 # Set the number of iterations
-num_iterations <- 10000 # length(seeds)
+num_iterations <- 5 # length(seeds)
 n <- 1000000
 
 # Initialize a list to store the output (cumulative sums of duplicates)
-output_list <- vector("list", length = num_iterations)
+output_list_32 <- vector("list", length = num_iterations)
+output_list_64 <- numeric(num_iterations)
 
 # Record the start time
 start_time <- Sys.time()
@@ -15,18 +18,21 @@ pb <- txtProgressBar(min = 0, max = num_iterations, style = 3)
 
 # Loop through the specified number of iterations
 for (i in 1:num_iterations) {
+  
   # Set a new seed for each iteration
   set.seed(seeds[i])
+  dqset.seed(seeds[i])
   
   # Generate random numbers
-  U <- runif(n)
+  U_32 <- runif(n)
+  U_64 <- dqrunif(n)
   
   # Identify duplicates
-  I <- duplicated(U)
+  I_32 <- duplicated(U_32)
   
   # Store the cumulative sums in the list
-  output_list[[i]] <- cumsum(I)
-  
+  output_list_32[[i]] <- cumsum(I_32)
+  output_list_64[i] <- sum(duplicated(U_64))
   # Increment the progress bar
   setTxtProgressBar(pb, i)
   
@@ -46,9 +52,12 @@ for (i in 1:num_iterations) {
 close(pb)
 
 # Convert to data frame with sequential column names
-output_df <- as.data.frame(output_list)
-colnames(output_df) <- 1:ncol(output_df)
+output_df_32 <- as.data.frame(output_list_32)
+colnames(output_df_32) <- 1:ncol(output_df_32)
 
 # Save the data frame to a CSV file
-output_file <- file.path("..", "data", "collisions", "output_list_R.csv")
-write.csv(output_df, file = output_file, row.names = FALSE)
+output_file_32 <- file.path("..", "data", "collisions", "output_list_32_R.csv")
+write.csv(output_df, file = output_file_32, row.names = FALSE)
+
+output_file_64 <- file.path("..", "data", "collisions", "output_array_64_R.csv")
+write.csv(output_list_64, file = output_file_64, row.names = FALSE)
