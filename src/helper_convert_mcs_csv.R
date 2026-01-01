11965c03812ac3c078ffbe94cf91b3862592d888
crahal <charlierahal@gmail.com>
2026-01-01 18:53:49 +0000

    Delete redundant things

--- src/helper_convert_mcs_csv.R
+++
@@ -1,7 +0,0 @@
-# set_wd to ./seeds/data/results/mcs first!
-
-filenames = dir(pattern="*.rda")
-for (i in filenames){
-  dat <- load(i)
-  write.csv(results_mcs_sca_cm$effect, paste('../csv_files/', i, '.csv', sep=""))
-}
